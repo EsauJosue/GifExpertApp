a7a0151d83c185a5e0843f5573407a0b04be9e82
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2024-12-13 12:27:49 -0600

    Creando el componente de getGifs donde se conecta a la API de Gipfhy

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,36 +1,43 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory";
+import GifGrid from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-    const [categories, setCategories] = useState(['One Punch','Dragon Ball','Samurai X']); // se crea un arreglo de categorias
+    const [categories, setCategories] = useState(['One Punch']); // se crea un arreglo de categorias
+    //useState es un hook que se encarga de manejar el estado de un componente funcional
+    //categories es el estado inicial que contiene un array de tres elementos
+    //setCategories es una funcion que se encarga de modificar o actualizr el estado de categories
+    
     
     const onAddCategory = ( newCategory) => { // se crea una funcion para agregar una categoria
-        setCategories([newCategory,...categories]); // ...categories es para que no se pierda la informacion que ya tenia. Valorant es la nueva categoria
-
+        if (categories.includes(newCategory)) return; // si la categoria ya existe, no se agrega
+        setCategories([newCategory,...categories]); //setCategories es una funcion que recibe un arreglo con la nueva categoria y las categorias existentes
+        //setCategories crea un nuevo array
+        //[newCategory, ...categories]: Agrega la nueva categoría al principio del array, mientras conserva las categorías existentes usando el operador spread (...).
+        // onAddCategory: Es una función que agrega una nueva categoría al estado categories.
+        // newCategory: Es el valor que será recibido desde el hijo (AddCategory).
     }
     return (
         <>
-        {/* titulo */}
             <h1>GifExpertApp</h1>
-        {/* input */}
-        {/* listado de Gifs */}
-        {/* Agregar categoria al dar clic al boton */}
             <AddCategory 
-            
-                // setCategories={ setCategories } 
-                onNewCategory={ (value) => onAddCategory(value) }
-                
+                onNewCategory =
+                { 
+                    (value) => onAddCategory(value) 
+                }
             />
-     
-        {/* Guardar lo que escriben en el input */}
-
-        <ol>
-           { categories.map( category => { //.map recorre el arreglo
-                return <li key={category}>{category}</li>
-           }) }
-        </ol>
-            {/* Gif Item */}
-        
+            
+                { 
+                    categories.map( category =>  //.map recorre el arreglo
+                            (
+                                <GifGrid 
+                                key={ category } 
+                                category={ category } 
+                                /> 
+                                //el proptype category recibe el valor de la categoria. Su sintaxis es category={ category }. El key sirve para identificar de manera única a cada elemento del arreglo.
+                            )
+                        ) 
+                }
         </>
     )
     }
